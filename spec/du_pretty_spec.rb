21bedddd23445462113a2772f032cb5915c92691
Katsuma Narisawa <face.the.music721@gmail.com>
2018-09-04 23:07:18 +0900

    Add tests

--- spec/du_pretty_spec.rb
+++ spec/du_pretty_spec.rb
@@ -28,37 +28,67 @@ RSpec.describe DuPretty do
     expect(DuPretty::VERSION).not_to be nil
   end
 
-  subject do
-    DuPretty::DuWrapper.new(
-      path,
-      depth: depth,
-      min_kbyte: min_kbyte,
-    ).tree.split("\n").length
-  end
+  describe '#tree' do
+    subject do
+      DuPretty::DuWrapper.new(
+        path,
+        depth: depth,
+        min_kbyte: min_kbyte,
+      ).tree.split("\n").length
+    end
 
-  it "works" do
-    expect(subject).to eq(6)
-  end
+    it "works" do
+      expect(subject).to eq(6)
+    end
 
-  context 'with depth argument' do
-    let(:depth) { 2 }
-    it "filter output by depth" do
-      expect(subject).to eq(4)
+    context 'with depth argument' do
+      let(:depth) { 2 }
+      it "filter output by depth" do
+        expect(subject).to eq(4)
+      end
+    end
+
+    context 'with min_kbyte argument' do
+      let(:min_kbyte) { 200 }
+      it "filter output by minimum kbyte" do
+        expect(subject).to eq(2)
+      end
+    end
+
+    context 'with min_kbyte and depth arguments' do
+      let(:min_kbyte) { 110 }
+      let(:depth) { 2 }
+      it "filter output by minimum kbyte" do
+        expect(subject).to eq(3)
+      end
     end
   end
 
-  context 'with min_kbyte argument' do
-    let(:min_kbyte) { 200 }
-    it "filter output by minimum kbyte" do
-      expect(subject).to eq(2)
+  describe '#sorted' do
+    subject do
+      DuPretty::DuWrapper.new(
+        path,
+        depth: depth,
+        min_kbyte: min_kbyte,
+      ).sorted.split("\n").length
+    end
+
+    it "works" do
+      expect(subject).to eq(6)
     end
   end
 
-  context 'with min_kbyte and depth arguments' do
-    let(:min_kbyte) { 110 }
-    let(:depth) { 2 }
-    it "filter output by minimum kbyte" do
-      expect(subject).to eq(3)
+  describe '#original' do
+    subject do
+      DuPretty::DuWrapper.new(
+        path,
+        depth: depth,
+        min_kbyte: min_kbyte,
+      ).original.split("\n").length
+    end
+
+    it "works" do
+      expect(subject).to eq(6)
     end
   end
 end
