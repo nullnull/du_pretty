0038526111254c050c03aec3530fbead0ca11a96
Katsuma Narisawa <face.the.music721@gmail.com>
2018-09-04 20:16:39 +0900

    Implement DuWrapper (#2)
    * Update settings for Thor to enable to use argument
    
    * Install colorize
    
    * Implement DuWrapper
    
    * Add spec

--- lib/du_pretty.rb
+++ lib/du_pretty.rb
@@ -1,6 +1,90 @@
+require 'colorize'
+
 require 'du_pretty/version'
 require 'du_pretty/cli'
 
 module DuPretty
-  # Your code goes here...
+  class DuWrapper
+    def initialize(path, min_kbyte: 0, depth: nil)
+      @path = File.expand_path(path, Pathname.pwd)
+      @min_kbyte = min_kbyte
+      @depth = depth
+    end
+
+    def pretty
+      du.split("\n")
+        .map { |line| DiskUsage.new(line, @path) }
+        .select { |x| x.kbyte >= @min_kbyte }
+        .reverse
+        .map(&:pretty)
+        .join("\n")
+    end
+
+    private
+
+    def du
+      options = @depth.nil? ? '' : "-d #{@depth}"
+      `du -k #{options} #{@path}`
+    end
+
+    class DiskUsage
+      attr_accessor :raw, :kbyte, :path
+
+      def initialize(raw, root)
+        @raw = raw
+        @root = root
+        @kbyte = raw.split("\t").compact[0].to_i
+        @path = raw.split("\t").compact[1]
+      end
+
+      def pretty
+        pretty_path + ' ' + pretty_byte
+      end
+
+      private
+
+      def basename
+        Pathname.new(@path).basename.to_s
+      end
+
+      def depth
+        Pathname.new(relative_path).each_filename.to_a.size - 1
+      end
+
+      def pretty_byte
+        mb = @kbyte / 1024
+        gb = mb / 1024
+        if gb.positive?
+          if gb > 10
+            "#{gb}GB".red
+          else
+            "#{gb}GB".light_red
+          end
+        elsif mb.positive?
+          if mb > 500
+            "#{mb}MB".yellow
+          else
+            "#{mb}MB".light_yellow
+          end
+        else
+          if @kbyte > 500
+            "#{@kbyte}KB".green
+          else
+            "#{@kbyte}KB".light_green
+          end
+        end
+      end
+
+      def pretty_path
+        return '.' if @path == @root
+        spaces = depth.positive? ? '  ' * depth : ''
+        tree_symbol = '└── '
+        spaces + tree_symbol + basename
+      end
+
+      def relative_path
+        @path.sub(/^#{@root}/, '')
+      end
+    end
+  end
 end
--- lib/du_pretty/cli.rb
+++ lib/du_pretty/cli.rb
@@ -3,10 +3,20 @@ require 'thor'
 
 module DuPretty
   class CLI < Thor
-    default_command :search
-    desc 'search', 'search is my default_command'
-    def search
-      puts 'hello world!'
+    default_task :path
+    desc 'path', ''
+    option :min_gbyte, type: :numeric
+    option :min_mbyte, type: :numeric
+    option :min_kbyte, type: :numeric
+    option :depth, type: :numeric, aliases: :d
+    def path(path = '.')
+      min_kbyte = [(options[:min_gbyte] || 0) * 1024 * 1024, (options[:min_mbyte] || 0) * 1024, (options[:min_kbyte] || 0)].max
+      print DuWrapper.new(
+        path,
+        min_kbyte: min_kbyte,
+        depth: options[:depth]
+      ).pretty
+      print "\n"
     end
   end
 end
