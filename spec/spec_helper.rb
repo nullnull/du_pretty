25cc91d047eb4563a410ff2fa0dbced5a6114245
Katsuma Narisawa <face.the.music721@gmail.com>
2018-09-05 01:01:32 +0900

    Install and configure simplecov

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,9 +1,11 @@
-require "bundler/setup"
-require "du_pretty"
+require 'bundler/setup'
+require 'du_pretty'
+require 'simplecov'
+SimpleCov.start
 
 RSpec.configure do |config|
   # Enable flags like --only-failures and --next-failure
-  config.example_status_persistence_file_path = ".rspec_status"
+  config.example_status_persistence_file_path = '.rspec_status'
 
   # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
